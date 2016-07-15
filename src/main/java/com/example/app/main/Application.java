958f5c723699884d8f4f796fd7bde5ee12b66a1f
byeongsu kim <kimbs0301@gmail.com>
2016-07-15 22:05:50 +0900

    ok

--- src/main/java/com/example/app/main/Application.java
+++ src/main/java/com/example/app/main/Application.java
@@ -4,6 +4,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.PropertyPlaceholderAutoConfiguration;
+import org.springframework.boot.autoconfigure.admin.SpringApplicationAdminJmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
+import org.springframework.boot.autoconfigure.context.ConfigurationPropertiesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.HttpEncodingAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.HttpMessageConvertersAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.MultipartAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.ServerPropertiesAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.WebSocketAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.WebSocketMessagingAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
@@ -17,12 +36,30 @@ import com.example.spring.config.WebAppContextConfig;
  * 
  */
 @Configurable
+@EnableAutoConfiguration(exclude = { PropertyPlaceholderAutoConfiguration.class, //
+		EmbeddedServletContainerAutoConfiguration.class, //
+		JmxAutoConfiguration.class, //
+		SpringApplicationAdminJmxAutoConfiguration.class, //
+		AopAutoConfiguration.class, //
+		TransactionAutoConfiguration.class, //
+		WebSocketAutoConfiguration.class, //
+		WebSocketMessagingAutoConfiguration.class, //
+		DispatcherServletAutoConfiguration.class, //
+		ErrorMvcAutoConfiguration.class, //
+		ServerPropertiesAutoConfiguration.class, //
+		JacksonAutoConfiguration.class, //
+		DataSourceAutoConfiguration.class, //
+		HttpMessageConvertersAutoConfiguration.class, //
+		DataSourceTransactionManagerAutoConfiguration.class, //
+		ConfigurationPropertiesAutoConfiguration.class, //
+		HttpEncodingAutoConfiguration.class, //
+		MultipartAutoConfiguration.class })
 @ComponentScan(basePackages = { "com.example.spring" }, basePackageClasses = { EmbeddedTomcatConfig.class }, excludeFilters = {
 		@Filter(value = { WebAppContextConfig.class }, type = FilterType.ASSIGNABLE_TYPE),
 		@Filter(pattern = { "com.example.spring.*.model.*" }, type = FilterType.REGEX) })
 public class Application {
 	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
-	
+
 	public Application() {
 		LOGGER.debug("생성자 Application()");
 	}
